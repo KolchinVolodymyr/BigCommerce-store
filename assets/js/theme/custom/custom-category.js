870dd190a3711650c098696dc9358bb7e86107e8
Volodymyr Kolchin <vkolchin@codifi.me>
2021-10-25 17:02:06 +0300

    All quantity fields default to 0 on the initial page load

--- assets/js/theme/custom/custom-category.js
+++ assets/js/theme/custom/custom-category.js
@@ -6,8 +6,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ProductItem from './reactComponent/productItemOrder';
 import ProductTotal from "./reactComponent/productOrderTotal";
-import nod from "nod-validate";
 import { showAlertModal } from '../global/modal';
+import nod from "nod-validate";
 
 export default class CustomCategory extends PageManager {
     constructor(context) {
@@ -18,12 +18,16 @@ export default class CustomCategory extends PageManager {
         this.$totalContainer = $('#total')[0];
         this.$input = $('.form-input-order');
         this.total = 0;
+        this.preTotal = 0;
         this.currencyCode = null;
         this.products = [];
+        this.stock_level = null;
         this.$addToCartBtnAbove = $('#addToCart-above');
         this.$addToCartBtnBelow = $('#addToCart-below');
         this.$addToCartBtnAbove.on('click', () => this.customAddToCartButton());
         this.$addToCartBtnBelow.on('click', () => this.customAddToCartButton());
+        this.Nod = nod();
+        this.cartItemsID = '';
     }
 
     /**
@@ -31,15 +35,74 @@ export default class CustomCategory extends PageManager {
     *
     */
     getProduct(productID) {
-        get (`{site { products(entityIds: [${productID}]) { edges{ node{ id, entityId, name, description, sku, inventory {isInStock}, createdAt {utc} prices { price { value, currencyCode } } defaultImage { url(width:1280) } } } } } }`)
+        get (`{site { products(entityIds: [${productID}]) { edges {
+                            node {
+                                id,
+                                entityId,
+                                name,
+                                description,
+                                sku,
+                                productOptions {
+                                    edges {
+                                      node {
+                                        entityId
+                                        displayName
+                                        ... on MultipleChoiceOption {
+                                          displayStyle
+                                          values {
+                                            edges {
+                                              node {
+                                                entityId
+                                                label
+                                              }
+                                            }
+                                          }
+                                        }
+                                      }
+                                    }
+                                  }
+                                variants(first: 100) {
+                                    edges {
+                                      node {
+                                        sku
+                                        prices {
+                                            price {
+                                                value
+                                                currencyCode
+                                              }
+                                        }
+                                      }
+                                    }
+                                  }
+                                inventory {
+                                    aggregated {
+                                        availableToSell
+                                    }
+                                }
+                                prices {
+                                    price { value, currencyCode }
+                                }
+                                defaultImage { url(width:1280) }
+                            }
+                        }
+                     }
+                   }
+                 }`)
             .then((data) => {
-                this.$addToCartBtnAbove.prop('disabled', true);
-                this.$addToCartBtnBelow.prop('disabled', true);
                 this.products = data.site.products;
-                ReactDOM.render(<ProductItem data={data} onChange={this.onChange}/>, this.$container);
+                ReactDOM.render(<ProductItem data={data} onChange={this.onChange.bind(this)}/>, this.$container);
                 this.amountProduct(data);
                 ReactDOM.render(<ProductTotal total={this.total} currencyCode={this.currencyCode}/>, this.$totalContainer);
-            });
+            }).then(()=>{
+                this.products.forEach(el => {
+                    el.productOptions.forEach(i=>{
+                        document.getElementById(`${i.entityId}`).addEventListener('change', function (e){
+                            el.optionSelections = [];
+                            el.optionSelections.push({ "optionId": i.entityId, "optionValue": this.value});
+                        });
+                    });
+                })
+            })
     }
 
     /**
@@ -48,30 +111,60 @@ export default class CustomCategory extends PageManager {
     */
     onChange(e) {
         const $input = $(e.target);
-        nod().configure({
-            submit: 'button--primary',
-            disableSubmit: true
-        });
-        nod().add([{
+        this.Nod.add([{
             // Raw dom element
             selector: $input,
             // Custom function. Notice that a call back is used. This means it should
             // work just fine with ajax requests (is user name already in use?).
             validate: function (callback, value) {
                 if(!isNaN(value) && value<=10) {
-                    $input.removeClass("form-field--error");
-                    callback(true);
-                    $('#addToCart-above')[0].removeAttribute("disabled");
-                    $('#addToCart-below')[0].removeAttribute("disabled");
+                    if(this.total == 0) {
+                        callback(true);
+                        this.$addToCartBtnAbove.prop('disabled', true);
+                        this.$addToCartBtnBelow.prop('disabled', true);
+                    } else {
+                        if(isNaN(this.preTotal)) {
+                            callback(true);
+                            this.$addToCartBtnAbove.prop('disabled', true);
+                            this.$addToCartBtnBelow.prop('disabled', true);
+                        } else {
+                            callback(true);
+                            this.$addToCartBtnAbove.prop('disabled', false);
+                            this.$addToCartBtnBelow.prop('disabled', false);
+                        }
+                    }
                 } else {
-                    $input.addClass("form-field--error");
                     callback(false);
-                    $('#addToCart-above')[0].setAttribute("disabled", "");
-                    $('#addToCart-below')[0].setAttribute("disabled", "");
+                    this.$addToCartBtnAbove.prop('disabled', true);
+                    this.$addToCartBtnBelow.prop('disabled', true);
                 }
-            },
+            }.bind(this),
             errorMessage: 'Please enter a valid product quantity(from 0 to 10)'
+        },{
+            selector: $input,
+            validate: function (callback, value) {
+                if($input.context.dataset?.productStock == undefined) {
+                    callback(true);
+                } else {
+                    if(value <=  $input.context.dataset?.productStock) {
+                        callback(true);
+                        if( $('.form-field--error').length >= 1 ){
+                            this.$addToCartBtnAbove.prop('disabled', true);
+                            this.$addToCartBtnBelow.prop('disabled', true);
+                        } else {
+                            this.$addToCartBtnAbove.prop('disabled', false);
+                            this.$addToCartBtnBelow.prop('disabled', false);
+                        }
+                    } else {
+                        callback(false);
+                        this.$addToCartBtnAbove.prop('disabled', true);
+                        this.$addToCartBtnBelow.prop('disabled', true);
+                    }
+                }
+            }.bind(this),
+            errorMessage: `We dont have enough stock on. Available quantity ${$input.context.dataset.productStock}. Please try again.`
         }]);
+        this.Nod.performCheck();
     };
 
     /**
@@ -81,7 +174,7 @@ export default class CustomCategory extends PageManager {
     amountProduct(data) {
         data.site.products.forEach(el => {
             el.count = 0;
-            document.getElementById(`${el.entityId}`).addEventListener("input", function(e) {
+            document.getElementById(`${el.entityId}`).addEventListener("input", function() {
                 el.count = document.getElementById(`${el.entityId}`).value;
                 el.sumProduct = el.count*el.prices.price.value;
                 this.sum(data.site.products);
@@ -97,12 +190,15 @@ export default class CustomCategory extends PageManager {
     */
     sum(products) {
         let priceArr = [];
+        let arr = [];
         products.forEach(el=> {
+            arr.push(el.sumProduct);
             if(!isNaN(el.sumProduct)) {
                 priceArr.push(el.sumProduct);
             }
         });
-        this.total = priceArr.reduce((previousValue, currentValue) => {return previousValue + currentValue}, 0);
+        this.total = priceArr.reduce((previousValue, currentValue) => {return previousValue + currentValue}, 0).toFixed(2);
+        this.preTotal = arr.reduce((previousValue, currentValue) => {return previousValue + currentValue}, 0);
         ReactDOM.render(<ProductTotal total={this.total} currencyCode={this.currencyCode}/>, this.$totalContainer);
     }
 
@@ -112,30 +208,82 @@ export default class CustomCategory extends PageManager {
     *
     */
     async customAddToCartButton () {
-        for(const product of this.products) {
-            this.$overlay.show();
-            await fetch(`/cart.php?action=add&product_id=${product.entityId}&qty=${product.count}`)
-                .then((res) => {
-                    if (res.status >= 200 && res.status < 300) {
-                        return res;
-                    } else {
-                        let error = new Error(res.statusText);
-                        error.response = res;
-                        throw error
+        //let productCount = document.getElementById('qty[]').value;
+        if (this.Nod.areAll('valid')) {
+            if (this.total !== 0) {
+                this.$overlay.show();
+                for (const product of this.products) {
+                    if(product.count !==0 && product.productOptions.length!==0) {
+                        await this.createCartItems(`/api/storefront/carts/${this.cartItemsID ? `${this.cartItemsID}/item` : ''}`, {
+                            "lineItems": [{
+                                "quantity": product.count,
+                                "productId": product.entityId,
+                                "optionSelections":  product.optionSelections
+                            }]
+                        }).then(cart => {
+                            this.cartItemsID = cart?.id;
+                        })
                     }
-                })
-                .catch(error => showAlertModal(error))
-                .finally(() => {
-                    this.$overlay.hide();
-                });
+                    if(product.count !==0 && product.productOptions.length==0) {
+                         await this.createCartItems(`/api/storefront/carts/${this.cartItemsID ? `${this.cartItemsID}/item` : ''}`, {
+                                "lineItems": [{
+                                    "quantity": product.count,
+                                    "productId": product.entityId
+                                }]
+                         }).then(cart => {
+                             this.cartItemsID = cart?.id;
+                         })
+                    }
+                }
+                this.$overlay.hide();
+                window.location = '/cart.php'
+            } else {
+                showAlertModal('корзина пустая, добавьте товар в корзину');
+            }
+        } else {
+            showAlertModal('введите, валидные данные');
         }
-        // go to cart.
-        window.location = "/cart.php";
     }
 
+    /**
+     * Returns a Cart
+     */
+    getCart(url) {
+        return fetch(url, {
+            method: "GET",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(cart => {
+                this.cartItemsID = cart[0]?.id
+            })
+            .catch(error => console.error(error));
+    }
+    /**
+     * Creates a Cart
+     */
+    createCartItems(url, cartItems) {
+        return fetch(url, {
+            method: "POST",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(cartItems),
+        }).then(response => response.json());
+    };
+
     onReady() {
+        this.$addToCartBtnAbove.prop('disabled', true);
+        this.$addToCartBtnBelow.prop('disabled', true);
+        this.getCart(`/api/storefront/carts`);
+
         this.context.products.forEach(element => {
             this.productsId.push(element.id);
+            //this.stock_level = element.stock_level;
             this.currencyCode = element.price.without_tax.currency;
         });
         this.getProduct(this.productsId);
--- assets/js/theme/custom/reactComponent/productItemOrder.js
+++ assets/js/theme/custom/reactComponent/productItemOrder.js
@@ -9,10 +9,30 @@ export default function ProductItem({data, onChange, checkParams}) {
                         <div className="item-col-1">
                             <h2 className="item-title-product">{el.name}</h2>
                             <img src={el.defaultImage.url} alt=""/>
+                            <div className="description">
+                                {el.description.replace(/<[^>]+>/g, '').substr(0, 240)}
+                            </div>
                         </div>
                         <div className="item-col-2">
-                            <div className="description">
-                                {el.description.replace(/<[^>]+>/g, '')}
+                            <div className="price">
+                                {el.prices.price.value} {el.prices.price.currencyCode}
+                            </div>
+                            <div className="productOption">
+                                {el.productOptions.map((i, indexOptions)=> {
+                                    return (
+                                        <div key={indexOptions}>
+                                        <div>{i.displayName}</div>
+                                            <select id={i.entityId} className="form-select form-select--small">
+                                            {i.values.map((a, indexLabel)=>{
+                                                return (
+                                                        <option value={a.entityId} key={indexLabel} >{a.label}</option>
+                                                    )
+                                                })}
+                                            </select>
+                                        </div>
+                                    )
+                                })
+                                }
                             </div>
                         </div>
                         <div className="item-col-3">
@@ -23,6 +43,8 @@ export default function ProductItem({data, onChange, checkParams}) {
                                     name="country"
                                     type="text"
                                     onChange={onChange}
+                                    placeholder="0"
+                                    data-product-stock={el.inventory.aggregated?.availableToSell}
                                 />
                             </div>
                         </div>
