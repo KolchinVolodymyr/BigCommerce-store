12840e40add64001f10d71606f0f2285ff9c4969
Volodymyr Kolchin <vkolchin@codifi.me>
2021-10-26 08:56:02 +0300

    I added multiple-order-page.html

---
+++ assets/js/theme/custom/multiple-order-page.js
@@ -0,0 +1,10 @@
+import PageManager from '../page-manager'
+import 'regenerator-runtime/runtime';
+import React from 'react';
+
+export default class CustomOrderPage extends PageManager {
+
+    onReady() {
+        console.log('hello')
+    }
+}
