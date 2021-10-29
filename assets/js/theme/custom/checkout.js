14b9c821549acc7390d7d8ada3950360ac59dcac
Volodymyr Kolchin <vkolchin@codifi.me>
2021-10-29 17:55:58 +0300

    setting up an event listener

--- assets/js/theme/custom/checkout.js
+++ assets/js/theme/custom/checkout.js
@@ -1,15 +1,36 @@
 import PageManager from '../page-manager';
-//import initApolloClient from '../global/graphql/client';
-//import customerAttributesQuery from './gql/customerAttributes.gql';
-//import flattenGraphQLResponse from 'humanize-graphql-response';
+import initApolloClient from '../global/graphql/client';
+import customerData from './gql/customerData.gql';
+import flattenGraphQLResponse from 'humanize-graphql-response';
 
 export default class CustomCheckout extends PageManager {
     constructor(context) {
         super(context);
+        this.gqlClient = initApolloClient(this.context.storefrontAPIToken);
+
+        const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+        this.observer = new MutationObserver((this.callback).bind(this));
+        this.observer.observe(window.document.body, {
+            childList: true,
+            subtree: true,
+        });
+    }
+    callback (changes, observer) {
+        console.log(changes);
+        console.log(observer);
     }
 
     onReady() {
-       console.log('hello world');
-       console.log('this', this);
+
+        console.log('this', this);
+
+        this.gqlClient.query({
+            query: customerData,
+        }).then(res => {
+           //console.log('res', res);
+            const paymentMethodLabel = flattenGraphQLResponse(res)?.data?.customer?.attributes?.paymentMethodLabel?.value;
+            console.log('paymentMethodLabel', paymentMethodLabel);
+        })
+
     }
 }
