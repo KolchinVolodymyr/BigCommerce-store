a75ce6e3f8b28a67c70c3df87532d5c5b1c7d686
Volodymyr Kolchin <vkolchin@codifi.me>
2021-11-16 14:51:45 +0200

    Every customer has a different set of products displayed for them

--- assets/js/theme/custom/bulk-order.js
+++ assets/js/theme/custom/bulk-order.js
@@ -1,8 +1,63 @@
 import PageManager from '../page-manager';
+import initApolloClient from '../global/graphql/client';
+import customerData from './gql/customerData.gql';
+import getProductsSKU from './gql/getProductsSKU.gql';
 
 export default class CustomBulkOrder extends PageManager {
+    constructor(context) {
+        super(context);
+        this.gqlClient = initApolloClient(this.context.storefrontAPIToken);
+        this.productSKUsArray = null;
+        this.productsList = [];
+    }
+
     onReady() {
-        console.log('hello');
+        this.gqlClient.query({
+            query: customerData,
+        }).then(res => {
+            this.productSKUsArray = res.data.customer.attributes.productBulkOrderList.value.replace(/\s/g, '').split(',');
+            console.log('this.productSKUsArray', this.productSKUsArray);
+            this.getProductsData(this.productSKUsArray);
+        })
+    }
+
+
+    /**
+     *
+     * @param {String} productSkuItem
+     */
+    productsSKU(productSkuItem) {
+        this.gqlClient.query({
+            query: getProductsSKU,
+            variables: { sku: productSkuItem },
+        }).then(res => {
+            this.productsList.push(res.data.site.product);
+        })
+    }
+
+    /**
+     *
+     * @param {Array} productSKUs
+     */
+     getProductsData(productSKUs){
+        this.forEachPromise(productSKUs)
+            .then(() => {
+                console.log('done', this.productsList);
+                //React render
+            });
+     }
+
+    /**
+     *
+     * @param items An array of items.
+     * @returns {Promise}
+     */
+    forEachPromise(items) {
+        return items.reduce(function (promise, item) {
+            return promise.then(function () {
+                return this.productsSKU(item);
+            }.bind(this));
+        }.bind(this), Promise.resolve());
     }
 
 }
