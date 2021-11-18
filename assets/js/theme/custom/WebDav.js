69a688436f23671e83710c7bee1a864b9ac44120
Volodymyr Kolchin <vkolchin@codifi.me>
2021-11-18 15:31:11 +0200

    fix bug

--- assets/js/theme/custom/WebDav.js
+++ assets/js/theme/custom/WebDav.js
@@ -2,26 +2,22 @@ import PageManager from '../page-manager';
 import $ from 'jquery';
 
 export default class CustomDemoWebDaw extends PageManager {
-  constructor(context) {
-    super(context);
-    this.productSku = $('.productView-info-value[data-product-sku]')[0].childNodes[0].data;
-  }
-  /**
-   *
-   */
-  renderWebDav() {
-    if ($('#webDavContent').length > 0) {
-      $('#webDavContent').load(`/content/product-information/${this.productSku}.html`, function( response, status, xhr){
-        if ( status == "error" ) {
-          $('#titleWebDav').hide();
-          $('#webDavID').hide();
-        } else {
-          $('#titleWebDav').show();
-        }
-      });
+    constructor(context) {
+        super(context);
+        this.productSku = $('.productView-info-value[data-product-sku]')[0].childNodes[0].data;
     }
-  }
-  onReady () {
-    this.renderWebDav();
-  }
+
+    renderWebDav() {
+        $('#webDavContent').load(`/content/product-information/${this.productSku}.html`, function( response, status, xhr){
+            if ( status == "error" ) {
+                $('#titleWebDav').hide();
+                $('#webDavID').hide();
+            } else {
+                $('#titleWebDav').show();
+            }
+        });
+    }
+    onReady () {
+        this.renderWebDav();
+    }
 }
