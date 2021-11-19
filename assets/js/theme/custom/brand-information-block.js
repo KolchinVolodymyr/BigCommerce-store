bba112ab78562e25aa87a7501e177a304a4106d4
Volodymyr Kolchin <vkolchin@codifi.me>
2021-11-19 13:29:32 +0200

    added React component

--- assets/js/theme/custom/brand-information-block.js
+++ assets/js/theme/custom/brand-information-block.js
@@ -2,15 +2,19 @@ import PageManager from '../page-manager';
 import initApolloClient from '../global/graphql/client';
 import brandInformationData from './gql/brandInformationData.gql';
 import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import BrandItem from './reactComponent/BrandItem';
 
 export default class CustomCategory extends PageManager {
     constructor(context) {
         super(context);
         this.gqlClient = initApolloClient(this.context.storefrontAPIToken);
         this.productId = this.context.productId;
+        this.$container = $('.productView-brand')[0];
     }
     /**
-     * Returns a list of product Variant Options.
+     * Returns a list of brand information.
      */
     async getOptions() {
         this.gqlClient
@@ -19,13 +23,12 @@ export default class CustomCategory extends PageManager {
             variables: { productId: parseInt(this.productId), },
             }).then((data) => {
                 console.log('data', data);
+                //ReactDOM.render(<BrandItem />, this.$container);
+                console.log(this.$container);
             });
     }
 
     onReady() {
-    console.log('s', this.context.storefrontAPIToken);
-        console.log('hello');
-        console.log(this.productId);
         this.getOptions()
     }
 }
